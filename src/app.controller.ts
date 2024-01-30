987267b4db1ef909fce30629f49343f3ba9fdb2a
Tushar <30565750+tushar5526@users.noreply.github.com>
2024-01-30 15:50:14 +0530

    feat: unify student data and file upload APIs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,38 @@
-import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Post, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { copyFileSync, unlinkSync } from 'fs';
 import { StudentDataDTO } from './StudentDataDTO';
 import { PrismaClient } from '@prisma/client';
+import { parse } from 'path';
 
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService,
     private readonly prisma: PrismaClient
   ) {}
 
-  @Post('file')
+  @Post('data')
   @UseInterceptors(FileInterceptor('file', {preservePath: true}))
-  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+  async uploadFile(@UploadedFile() file: Express.Multer.File, @Body() data: any) {
     try {
       copyFileSync(file.path, `${file.destination}/${file.originalname}`);
       unlinkSync(file.path);
+      await this.prisma.student_data.create({
+        data: {
+          name: data.name,
+          studentClass: data.studentClass,
+          schoolName: data.schoolName,
+          wronglyRecognizedCount: parseInt(data.wronglyRecognizedCount, 10),
+          wronglyRecognizedWords: data.wronglyRecognizedWords,
+          incorrectWordsCount: parseInt(data.incorrectWordsCount, 10),
+          incorrectWords: data.incorrectWords,
+          totalWords: parseInt(data.totalWords, 10),
+          totalTimeTaken: parseInt(data.totalTimeTaken, 10),
+          wordsPerMinute: parseInt(data.wordsPerMinute, 10),
+          audioFileName: file.originalname,
+          textReadId: parseInt(data.textReadId, 10)
+        }
+      });
       return {
         "success": true
       }
@@ -30,19 +45,4 @@ export class AppController {
     }
   }
 
-  @Post()
-  async uploadStudentData(@Body() data: StudentDataDTO) {
-    try {
-      await this.prisma.student_data.create({data});
-      return {
-        "success": true
-      }
-    }
-    catch (e) {
-      console.error(e);
-      return {
-        "success": false
-      }
-    }
-  }
 }
